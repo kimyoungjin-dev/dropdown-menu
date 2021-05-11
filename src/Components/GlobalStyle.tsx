fdf4402806f97875b9044361d983fadedc2b5289
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-11 18:26:29 +0900

    initialize css

---
+++ src/Components/GlobalStyle.tsx
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+
+    *{
+        box-sizing:border-box;
+        padding:0;
+        margin:0;
+    }
+
+    a{
+        text-decoration:none;
+        color:inherit;
+    }
+    
+    body{
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        background-color:white;
+        color:black;
+    }
+
+    input{
+        all:unset;
+    }
+`;
+
+export default globalStyles;
