028f277bf9b710b7b8524745d21adad29abc95cf
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-11 18:35:18 +0900

    It creates a navbar
    It receives data information and renders it on the screen.
    
    It delivers data to SubMenu.

--- src/Components/SliderMenu.tsx/SliderMenubar.tsx
+++ src/Components/SliderMenu.tsx/SliderMenubar.tsx
@@ -1,5 +1,60 @@
-import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { Data } from "./Data";
+import SubMenu from "./SubMenu";
+
+const Container = styled.div`
+  height: 80px;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const IConContainer = styled(Link)`
+  margin-left: 20px;
+  color: white;
+  font-size: 30px;
+`;
+
+const LeftContainer = styled.div<{ current: boolean }>`
+  width: 250px;
+  height: 100vh;
+  background-color: black;
+  position: fixed;
+  transition: 0.6s ease-in-out;
+  left: ${(props) => (props.current ? "0" : "-100%")};
+  z-index: 10;
+`;
+
+const Wrapper = styled.div``;
 
 export default function SliderMenubar() {
-  return <div></div>;
+  const [isClick, setIsClick] = useState(false);
+  const toggleSideVar = () => setIsClick((prev) => !prev);
+
+  return (
+    <>
+      <Container>
+        <IConContainer to="#">
+          <GiHamburgerMenu onClick={toggleSideVar} />
+        </IConContainer>
+      </Container>
+
+      <LeftContainer current={isClick}>
+        <Wrapper>
+          <IConContainer to="#">
+            <AiOutlineCloseCircle onClick={toggleSideVar} />
+          </IConContainer>
+
+          {Data.map((item, index) => (
+            <SubMenu key={index} item={item} />
+          ))}
+        </Wrapper>
+      </LeftContainer>
+    </>
+  );
 }
