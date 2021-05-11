a1e3189483af13f23119ac816aca8f4b17b1205c
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-11 18:34:27 +0900

    Apply GlobalStyle

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,14 @@
 import SliderMenubar from "./SliderMenu.tsx/SliderMenubar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import GlobalStyle from "./GlobalStyle";
 
 export default function App() {
   return (
-    <Router>
-      <SliderMenubar />
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <SliderMenubar />
+      </Router>
+    </>
   );
 }
